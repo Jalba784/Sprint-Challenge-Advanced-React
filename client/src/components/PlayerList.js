830a4009016604171c5644426d6665583570038b
josepaniagua <jpanjr@gmail.com>
2019-12-16 12:20:50 -0800

    configured cards to look orderly and painted container background red

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Player from "./Player";
+import { makeStyles } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 
+const useStyles = makeStyles({
+  cont: {
+    maxWidth: "86%",
+    backgroundColor: 'red',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+    alignItems: 'baseline',
+    alignContent: 'space-between',
+  }
+});
+
 const PlayerList = props => {
+  const classes = useStyles();
+  const { cont } = classes;
   const { playerData } = props;
 
   return (
     <div>
-      <Container>
+      <Container className={cont}>
         {playerData.map(person => {
           return <Player key={person.id} player={person} />;
         })}
